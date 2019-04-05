52c1ddbdad47f54d95cd0dca82ee5b1c1541e45a
maurinenami <maurinenami>
2019-04-05 16:50:40 +0300

    add new text in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("form#players").submit(function(event){
         event.preventDefault();
     })
-    var player 1 = $ (input#player)
+    var player1 = $ (input#player1name).val()
+    var player2 =$ (input#player2name).val()
 
 })
