1312bdcd7e6dcf16899a4a5636e89269151ab9b8
maurinenami <maurinenami>
2019-04-05 17:47:50 +0300

    add new text

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,19 @@ $(document).ready(function(){
     var player2 =$ (input#player2name).val()
 
 })
+$(document).ready(function(){
+    $("player1-roll").click(()=> {
+        var randomNo1 = Math.floor((Math.random() * 6) + 1);
+        $("die-roll-1").text("Dice roll :" + randomNo1);
+    });
+});
+$(document).ready(function(){
+    $("player2-roll").click(()=> {
+        var randomNo1 = Math.floor((Math.random() * 6) + 1);
+        $("die-roll-2").text("Dice roll :" + randomNo2);
+    });
+});
+
+
+
+
