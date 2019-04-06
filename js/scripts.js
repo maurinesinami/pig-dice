af3923954fd055446b1b9663bfa6876387546a7a
maurinenami <maurinenami>
2019-04-06 12:16:10 +0300

    add new text in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,13 @@
 $(document).ready(function(){
-    $("form#players").submit(function(event){
-        event.preventDefault();
-    })
-    var player1 = $ (input#player1name).val()
-    var player2 =$ (input#player2name).val()
-
-})
-$(document).ready(function(){
-    $("player1-roll").click(()=> {
-        var randomNo1 = Math.floor((Math.random() * 6) + 1);
-        $("die-roll-1").text("Dice roll :" + randomNo1);
+    $("#player1-roll").click(()=>{
+        var randomNO1 = Math.floor((Math.random() * 6)+ 1)
+        $("#die-roll-1").text( randomNO1);
     });
 });
 $(document).ready(function(){
-    $("player2-roll").click(()=> {
-        var randomNo1 = Math.floor((Math.random() * 6) + 1);
-        $("die-roll-2").text("Dice roll :" + randomNo2);
+    $("#player2-roll").click(()=>{
+        var randomNO2 = Math.floor((Math.random() * 6)+ 1)
+        $("#die-roll-2").text( randomNO2);
     });
 });
-
-
-
 
