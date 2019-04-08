d647f8e04c6d008999a17138f7942c025d6ec4de
maurinenami <maurinenami>
2019-04-08 09:04:05 +0300

    add new corrections of the round button

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 $("input:text").ready(function () {
   $("#START").click(() => {
     var nameOne = $("input:text#player1name").val();
-    $("#player1name").text("PLAYER 1 NAME " + nameOne);
+    $("#player1name").text("Player 1 name: " + nameOne);
 
     var nameTwo = $("input:text#player2name").val();
-    $("#player2name").text("PLAYER 2 NAME: " + nameTwo);
+    $("#player2name").text("Player 2 name: " + nameTwo);
   });
 });
 
@@ -21,12 +21,12 @@ $(document).ready(function () {
   $("#player1-roll").click(() => {
     var randomNO1 = Math.floor((Math.random() * 6) + 1)
     $("#die-roll-1").text(randomNO1);
-    if (random1 === 1) {
+    if (randomNO1 === 1) {
       round1Total = 0;
-      $("#round-total-1").text("Round total: " + round1Total);
+      $("#round-total-1").text( ""+round1Total);
     } else {
-      round1Total = random1 + round1Total;
-      $("#round-total-1").text("Round total: " + round1Total);
+      round1Total = randomNO1 + round1Total;
+      $("#round-total-1").text( ""+round1Total);
     }
   });
 });
@@ -35,26 +35,26 @@ $(document).ready(function () {
   $("#player2-roll").click(() => {
     var randomNO2 = Math.floor((Math.random() * 6) + 1)
     $("#die-roll-2").text(randomNO2);
-    if (random2 === 1) {
+    if (randomNO2 === 1) {
       round2Total = 0;
-      $("#round-total-2").text("Round total: " + round2Total);
+      $("#round-total-2").text( ""+round2Total);
     } else {
-      round2Total = random2 + round2Total;
-      $("#round-total-2").text("Round total: " + round2Total);
+      round2Total = randomNO2 + round2Total;
+      $("#round-total-2").text( ""+round2Total);
     }
   });
 });
 $(document).ready(function () {
-      $("#player1-hold").click(() => {
-        hold1 = round1Total + totalScore1;
-        round1Total = 0;
-        $("#totalScore").text("Total score" + hold1)
-      })
-    })
-    $(document).ready(function () {
-        $("#player2-hold").click(() => {
-          hold1 = round1Total + totalScore1;
-          round1Total = 0;
-          $("#totalScore").text("Total score" + hold2)
-        })
-      })
+  $("#player1-hold").click(() => {
+    hold1 = round1Total + totalScore1;
+    round1Total = 0;
+    $("#total-score-1").text("" + hold1)
+  })
+})
+$(document).ready(function () {
+  $("#player2-hold").click(() => {
+    hold2 = round2Total + totalScore2;
+    round2Total = 0;
+    $("#total-score-2").text("" + hold2)
+  });
+});
